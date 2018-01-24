724f6926cc8147084f81d4f00b357ef767907065
kaushik.prajapati <kaushik@indianic.com>
2018-01-24 13:45:17 +0530

    Card #1031: Turnover group query type

--- src/QueryType/AdministrationQueryType.php
+++ src/QueryType/AdministrationQueryType.php
@@ -15,45 +15,51 @@ class AdministrationQueryType extends AbstractPdoObjectType
             'name' => 'Administration',
             'fields' => function () use ($types) {
                 return [
-                'name' => [
-                    'type' => Type::string(),
-                    'description' => 'Administration Name',
-                ],
-                'accountName' => [
-                    'type' => Type::string(),
-                    'description' => 'Account Name',
-                ],
-                'currency' => [
-                    'type' => $types->get(CurrencyQueryType::class),
-                    'alias' => 'currencyId',
-                    'link' => 'getById',
-                    'description' => 'Currency',
-                ],
-                'description' => [
-                    'type' => type::string(),
-                    'description' => 'Description',
-                    'resolve' => function ($value) {
-                        return nl2br($value['description']);
-                    },
-                ],
-                'createdAt' => [
-                    'type' => Type::string(),
-                    'convert' => 'dateTimeToIsoDateTime',
-                    'description' => 'Created at',
-                ],
-                'deletedAt' => [
-                    'type' => Type::string(),
-                    'convert' => 'dateTimeToIsoDateTime',
-                    'description' => 'Deleted at',
-                ],
-                'stripeKey' => [
-                    'type' => Type::string(),
-                    'description' => 'Strip Key',
-                ],
-                'stripeSyncType' => [
-                    'type' => Type::string(),
-                    'description' => 'Strip Sync Type',
-                ],
+                    'name' => [
+                        'type' => Type::string(),
+                        'description' => 'Administration Name',
+                    ],
+                    'accountName' => [
+                        'type' => Type::string(),
+                        'description' => 'Account Name',
+                    ],
+                    'currency' => [
+                        'type' => $types->get(CurrencyQueryType::class),
+                        'alias' => 'currencyId',
+                        'link' => 'getById',
+                        'description' => 'Currency',
+                    ],
+                    'description' => [
+                        'type' => type::string(),
+                        'description' => 'Description',
+                        'resolve' => function ($value) {
+                            return nl2br($value['description']);
+                        },
+                    ],
+                    'createdAt' => [
+                        'type' => Type::string(),
+                        'convert' => 'dateTimeToIsoDateTime',
+                        'description' => 'Created at',
+                    ],
+                    'deletedAt' => [
+                        'type' => Type::string(),
+                        'convert' => 'dateTimeToIsoDateTime',
+                        'description' => 'Deleted at',
+                    ],
+                    'stripeKey' => [
+                        'type' => Type::string(),
+                        'description' => 'Strip Key',
+                    ],
+                    'stripeSyncType' => [
+                        'type' => Type::string(),
+                        'description' => 'Strip Sync Type',
+                    ],
+                    'TurnoverGroups' => [
+                        'type' => Type::listOf($types->get(TurnoverGroupQueryType::class)),
+                        'alias' => 'id',
+                        'list' => 'getAllByAdministrationId',
+                        'description' => 'Returns list of Turnover Group',
+                    ],
                 ];
             },
         ];
